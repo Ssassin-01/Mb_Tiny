ac2b4974fd429145d7bddf58573f18f864138145
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2025-04-21 12:12:51 +0900

    frontend Setting

---
+++ src/main/java/com/meeti/mbTiny/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.meeti.mbTiny.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
